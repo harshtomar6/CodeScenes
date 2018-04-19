11cddaccf4567e6d3e88425c490e6e975e454c64
Harsh Tomar <harshtomar6@gmail.com>
2018-04-19 23:37:33 +0530

    handled token decode

--- config.js
+++ config.js
@@ -6,20 +6,25 @@ let validateRequest = (req, res, next) => {
   let key = req.headers['x-key'];
 
   if(token && key){
-    let decoded = jwt.decode(token, process.env.SESSION_SECRET);
+    try{
 
-    if(decoded.exp <= Date.now()){
-      res.status(400).json({err: 'Token expired ! Login Again', data:null});
-      return;
-    }
+      let decoded = jwt.decode(token, process.env.SESSION_SECRET);
 
-    userController.getUserById(key, (err, status, data) => {
-      if(err){
-        res.status(status).send({err: err, data: null});
+      if(decoded.exp <= Date.now()){
+        res.status(400).json({err: 'Token expired ! Login Again', data:null});
+        return;
       }
-      else if(status == 200)
-        next()
-    });
+
+      userController.getUserById(key, (err, status, data) => {
+        if(err){
+          res.status(status).send({err: err, data: null});
+        }
+        else if(status == 200)
+          next()
+      });
+    }catch(err){
+      res.status(500).err({err: 'Something Weird happened', data: null})
+    }
   }else{
     res.status(401).json({err: 'Key Or Token Not Found', data: null});
   }
